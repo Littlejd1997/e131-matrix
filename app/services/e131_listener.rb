ec4842acc0e1f6a0d07c6539c6d2bd75fd0a87fe
root <root@server01.homelab>
2020-12-12 00:09:17 -0600

    a

--- app/services/e131_listener.rb
+++ app/services/e131_listener.rb
@@ -1,12 +1,22 @@
 class E131Listener
 
   def initialize
+    @framebuffer = []
     @socket = UDPSocket.new
     @socket.bind('0.0.0.0', 5568)
     call_block_when_data_recieved do |msg|
       e=E131::Packet.new(msg)
-      ActionCable.server.broadcast("matrix",
-        {universe: e.frame_layer.universe, values: e.dmp_layer.property_values.bytes[1..]})
+      
+      start_pixel = 170 * (e.frame_layer.universe - 1)
+      pixel_offset = 1
+      e.dmp_layer.property_values.bytes[1..].each_slice(3).each_with_index do |(red,green,blue), index|
+        @framebuffer[start_pixel + index] = "rgb(#{red},#{green},#{blue})"
+      end
+      if @framebuffer.size > 2500
+        ActionCable.server.broadcast("matrix",
+        {values: @framebuffer})
+      end      
+
       # puts msg[0].bytes.reject {|a| a == 0}.inspect
     end
 
